1611c13a79e0d64da937b7eb8db4f1b6db11e35f
webszy <sunzhenyu@live.com>
2020-10-22 09:10:40 +0800

    update
    import iconfont

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,16 +4,13 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
